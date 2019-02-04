9e20013b7bb3f182838de8483d2f9bffc7297b63
jarskijr <jarskijr@rose-hulman.edu>
2019-02-04 09:06:11 -0500

    done...ish

--- src/m2_fake_robot_as_mqtt_receiver.py
+++ src/m2_fake_robot_as_mqtt_receiver.py
@@ -1,4 +1,30 @@
-# TODO: Copy the code in
+
+import mqtt_remote_method_calls as com
+import time
+
+
+class DelegateThatReceives(object):
+
+    def say_it(self, message):
+        print("Message received!", message)
+
+
+def main():
+    name1 = input("Enter one name (subscriber): ")
+    name2 = input("Enter another name (publisher): ")
+
+    my_delegate = DelegateThatReceives()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect(name1, name2)
+    time.sleep(1)  # Time to allow the MQTT setup.
+    print()
+
+    while True:
+        time.sleep(0.01)  # Time to allow message processing
+
+
+main()
+# DONE: Copy the code in
 #     m1e_mqtt_receiver.py
 # as your starting point, pasting its code here.
 
